2c7d789dcab70ceaaa6691a802cb5fa762f0618a
Evelyn Lopez <127444209+EAntonellaLopez@users.noreply.github.com>
2023-10-18 10:29:40 -0300

    practica

--- envio.php
+++ envio.php
@@ -4,25 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="envio.php" method="post">
-    <label for="">Nombre</label>
-    <input name="nombre" type="text">
-    <br>
-    <label for="">Apellido</label>
-    <input name="apellido" type="text">
-    <br>
-    <label for="">Edad</label>
-    <input name="edad" type="text">
-    <br>
-    <select name="carrera" id="">
-        <option value="Comunicación multimedial">Comunicación multimedial</option>
-        <option value="Periodismo">Periodismo</option>
-        <option value="Cine">Cine</option>
-    </select>
-    <br>
-    <button type="submit">Envio</button>
-    </form>
 </body>
 </html>
+<?php 
+
+   $nombre = $_POST["nombre"];
+   $apellido = $_POST["apellido"];
+   $edad = $_POST["edad"];
+   $carrera = $_POST["carrera"];
+   echo"<div class=\"formulario\"> ";
+   echo" nombre {$nombre} ";
+   echo" apellido {$apellido} ";
+   echo" edad {$edad} ";
+   echo" carrera {$carrera} ";
+   echo "</div>";
+   echo "<a href=\"index.php\">Volver a inicio</a>";
+$connection = new SQLite3('estudiantes.db');
+
+    if (!$connection){
+
+        die("No se pudo conectar");
+
+    }
+    $connection->exec(
+
+        'CREATE TABLE IF NO EXISTS alumno(
+
+            id INTEGER PRIMARY KEY,
+            nombre TEXT,
+            apellido TEXT,
+            edad TEXT,
+            carrera TEXT
+        )'
+
+    );
+    $query = "INSERT INTO alumno (nombre,apellido,edad,carrera) 
+    VALUES ('$nombre','$apellido','$edad','$carrera')";
+
+    $resultado = $connection->exec($query);
+    if(!$resultado){
+        echo"Se guardo correctamente!";
+    }
+
+   ?>
--- index.php
+++ index.php
@@ -4,22 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Envio.php</h1>
+    <form action="envio.php" method="post">
+    <label for="">Nombre</label>
+    <input name="nombre" type="text">
+    <br>
+    <label for="">Apellido</label>
+    <input name="apellido" type="text">
+    <br>
+    <label for="">Edad</label>
+    <input name="edad" type="text">
+    <br>
+    <select name="carrera" id="">
+        <option value="Comunicación multimedial">Comunicación multimedial</option>
+        <option value="Periodismo">Periodismo</option>
+        <option value="Cine">Cine</option>
+    </select>
+    <br>
+    <button type="submit">Envio</button>
+    </form>
 </body>
 </html>
-<?php 
-   $nombre = $_POST["nombre"];
-   $apellido = $_POST["apellido"];
-   $edad = $_POST["edad"];
-   $carrera = $_POST["carrera"];
-   echo"<div class=\"formulario\"> ";
-   echo" nombre {$nombre} ";
-   echo" apellido {$apellido} ";
-   echo" edad {$edad} ";
-   echo" carrera {$carrera} ";
-   echo "</div>";
-   echo "<a href=\"index.php\">Volver a inicio</a>"
-   ?>
